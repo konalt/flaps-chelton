2bc02b03e235f61b2ab0bf43b15d09009cb83b11
big boy <danannlawless@gmail.com>
2022-05-29 15:38:17 +0100

    added some cards to cards against humanity - added uppercase stuff - added armstrong text

--- flapslib/cardsagainsthumanity.js
+++ flapslib/cardsagainsthumanity.js
@@ -6,9 +6,11 @@ var whiteCards = fs.readFileSync("./cah_white.txt").toString().split("\n");
 function get() {
     var chosenBlack = blackCards[Math.floor(Math.random() * blackCards.length)];
     var drawAmount = (chosenBlack.match(/____/g) || []).length;
+    var upper = (chosenBlack.toUpperCase() == chosenBlack);
     for (let i = 0; i < drawAmount; i++) {
         var chosen = whiteCards[Math.floor(Math.random() * whiteCards.length)];
         chosen = chosen.toLowerCase().replace(/\./g, "");
+        if (upper) chosen = chosen.toUpperCase();
         chosenBlack = chosenBlack.replace(/____/, chosen).replace(/(^\w{1}|(\.\?\!)\s*\w{1})/gi, function(toReplace) {
             return toReplace.toUpperCase();
         });
