ad6743898b6bcad4095482e5ed9b16ae0b1f3a01
big boy <danannlawless@gmail.com>
2022-08-25 15:32:50 +0100

    some changes ig

--- versus/run.js
+++ versus/run.js
@@ -37,11 +37,11 @@ async function ffmpeg(args) {
 
 function createBothVideo(input, output, options) {
     return ffmpeg(
-        `${verbose ? "" : "-v warning "}-y -loop 1 -i ${
-            input[0]
-        }.png -loop 1 -i ${
+        `${verbose ? "" : "-v warning "}-y ${
+            input[0].endsWith(".mp4") ? "" : "-loop 1 "
+        }-i ${input[0]} ${input[1].endsWith(".mp4") ? "" : "-loop 1 "}-i ${
             input[1]
-        }.png -t ${segmentLength} -i versus/black.png -filter_complex "[0:v]scale=400:390[img01_scaled];[1:v]scale=400:390[img02_scaled];[2:v]scale=400:800[bg_1s];[bg_1s][img01_scaled]overlay=0:0[bg_and_img01];[bg_and_img01][img02_scaled]overlay=0:410[vs_notext];[vs_notext]scale=400:800,setsar=1:1,setpts=PTS-STARTPTS[scaled];[scaled]split=1[scaled1];[scaled1]drawtext=text='${
+        } -t ${segmentLength} -i versus/black.png -filter_complex "[0:v]scale=400:390[img01_scaled];[1:v]scale=400:390[img02_scaled];[2:v]scale=400:800[bg_1s];[bg_1s][img01_scaled]overlay=0:0[bg_and_img01];[bg_and_img01][img02_scaled]overlay=0:410[vs_notext];[vs_notext]scale=400:800,setsar=1:1,setpts=PTS-STARTPTS[scaled];[scaled]split=1[scaled1];[scaled1]drawtext=text='${
             options[0]
         }':x=W/2-tw/2:y=H/3-th/2:fontsize=36:fontfile=font.ttf:borderw=5:bordercolor=black:fontcolor=white,drawtext=text='VS':x=W/2-tw/2:y=H/2-th/2:fontsize=66:fontfile=font.ttf:borderw=5:bordercolor=black:fontcolor=white,drawtext=text='${
             options[1]
@@ -51,31 +51,31 @@ function createBothVideo(input, output, options) {
 
 function createWinnerVideo(input, output, options) {
     return ffmpeg(
-        `${verbose ? "" : "-v warning "}-y -loop 1 -i ${
-            input[0]
-        }.png -loop 1 -i ${
+        `${verbose ? "" : "-v warning "}-y ${
+            input[0].endsWith(".mp4") ? "" : "-loop 1 "
+        }-i ${input[0]} ${input[1].endsWith(".mp4") ? "" : "-loop 1 "}-i ${
             input[1]
-        }.png -t ${segmentLength} -i versus/black.png -filter_complex "[0:v]scale=400:390[img01_scaled];[1:v]scale=400:390[img02_scaled];[2:v]scale=400:800[bg_1s];[bg_1s][img01_scaled]overlay=0:0[bg_and_img01];[bg_and_img01][img02_scaled]overlay=0:410[vs_notext];[vs_notext]scale=400:800,setsar=1:1,setpts=PTS-STARTPTS[scaled];[scaled]drawtext=text='Winner':x=W/2-tw/2:y=H/2-th/2:fontsize=66:fontfile=font.ttf:borderw=5:bordercolor=black:fontcolor=white,fps=25[out]" -map "[out]" -t ${segmentLength} -r 25 ${output}.mp4`
+        } -t ${segmentLength} -i versus/black.png -filter_complex "[0:v]scale=400:390[img01_scaled];[1:v]scale=400:390[img02_scaled];[2:v]scale=400:800[bg_1s];[bg_1s][img01_scaled]overlay=0:0[bg_and_img01];[bg_and_img01][img02_scaled]overlay=0:410[vs_notext];[vs_notext]scale=400:800,setsar=1:1,setpts=PTS-STARTPTS[scaled];[scaled]drawtext=text='Winner':x=W/2-tw/2:y=H/2-th/2:fontsize=66:fontfile=font.ttf:borderw=5:bordercolor=black:fontcolor=white,fps=25[out]" -map "[out]" -t ${segmentLength} -r 25 ${output}.mp4`
     );
 }
 
 function createTieVideo(input, output) {
     return ffmpeg(
-        `${verbose ? "" : "-v warning "}-y -loop 1 -i ${
-            input[0]
-        }.png -loop 1 -i ${
+        `${verbose ? "" : "-v warning "}-y ${
+            input[0].endsWith(".mp4") ? "" : "-loop 1 "
+        }-i ${input[0]} ${input[1].endsWith(".mp4") ? "" : "-loop 1 "}-i ${
             input[1]
-        }.png -t ${segmentLength} -i versus/black.png -filter_complex "[0:v]scale=400:390[img01_scaled];[1:v]scale=400:390[img02_scaled];[2:v]scale=400:800[bg_1s];[bg_1s][img01_scaled]overlay=0:0[bg_and_img01];[bg_and_img01][img02_scaled]overlay=0:410[vs_notext];[vs_notext]scale=400:800,setsar=1:1,setpts=PTS-STARTPTS[scaled];[scaled]drawtext=text='Tie':x=W/2-tw/2:y=H/2-th/2:fontsize=66:fontfile=font.ttf:borderw=5:bordercolor=black:fontcolor=white,fps=25[out]" -map "[out]" -t ${segmentLength} -r 25 ${output}.mp4`
+        } -t ${segmentLength} -i versus/black.png -filter_complex "[0:v]scale=400:390[img01_scaled];[1:v]scale=400:390[img02_scaled];[2:v]scale=400:800[bg_1s];[bg_1s][img01_scaled]overlay=0:0[bg_and_img01];[bg_and_img01][img02_scaled]overlay=0:410[vs_notext];[vs_notext]scale=400:800,setsar=1:1,setpts=PTS-STARTPTS[scaled];[scaled]drawtext=text='Tie':x=W/2-tw/2:y=H/2-th/2:fontsize=66:fontfile=font.ttf:borderw=5:bordercolor=black:fontcolor=white,fps=25[out]" -map "[out]" -t ${segmentLength} -r 25 ${output}.mp4`
     );
 }
 
 function createStatVideo(input, output, options) {
     return ffmpeg(
-        `${verbose ? "" : "-v warning "}-y -loop 1 -i ${
-            input[0]
-        }.png -loop 1 -i ${
+        `${verbose ? "" : "-v warning "}-y ${
+            input[0].endsWith(".mp4") ? "" : "-loop 1 "
+        }-i ${input[0]} ${input[1].endsWith(".mp4") ? "" : "-loop 1 "}-i ${
             input[1]
-        }.png -t ${segmentLength} -i versus/black.png -filter_complex "[0:v]scale=400:390[img01_scaled];[1:v]scale=400:390[img02_scaled];[2:v]scale=400:800[bg_1s];[bg_1s][img01_scaled]overlay=0:0[bg_and_img01];[bg_and_img01][img02_scaled]overlay=0:410[vs_notext];[vs_notext]scale=400:800,setsar=1:1,setpts=PTS-STARTPTS[scaled];[scaled]split=1[scaled1];[scaled1]drawtext=text='${
+        } -t ${segmentLength} -i versus/black.png -filter_complex "[0:v]scale=400:390[img01_scaled];[1:v]scale=400:390[img02_scaled];[2:v]scale=400:800[bg_1s];[bg_1s][img01_scaled]overlay=0:0[bg_and_img01];[bg_and_img01][img02_scaled]overlay=0:410[vs_notext];[vs_notext]scale=400:800,setsar=1:1,setpts=PTS-STARTPTS[scaled];[scaled]split=1[scaled1];[scaled1]drawtext=text='${
             options.stat
         }':x=W/2-tw/2:y=H/2-th/2:fontsize=72:fontfile=font.ttf:borderw=5:bordercolor=black:fontcolor=white,fps=25[out]" -map "[out]" -t ${segmentLength} -r 25 ${output}.mp4`
     );
@@ -83,9 +83,9 @@ function createStatVideo(input, output, options) {
 
 function createScoreVideo(input, output, options) {
     return ffmpeg(
-        `${
-            verbose ? "" : "-v warning "
-        }-y -loop 1 -t ${segmentLength} -i ${input}.png -filter_complex "[0:v]scale=-1:800,crop=400:800,setsar=1:1,setpts=PTS-STARTPTS[scaled];[scaled]drawtext=text='${
+        `${verbose ? "" : "-v warning "}-y ${
+            input.endsWith(".mp4") ? "" : "-loop 1 "
+        }-t ${segmentLength} -i ${input} -filter_complex "[0:v]scale=-1:800,crop=400:800,setsar=1:1,setpts=PTS-STARTPTS[scaled];[scaled]drawtext=text='${
             options.curScore
         }':x=W/2-tw/2:y=H/2-th/2:fontsize=72:fontfile=font.ttf:borderw=5:bordercolor=black:fontcolor=white,fps=25[out]" -map "[out]" -t ${segmentLength} -r 25 ${output}.mp4`
     );
@@ -134,103 +134,141 @@ async function todo(client, msg) {
         console.log("idsjijdfifjs");
         var att = msg.attachments.first(2);
         console.log(att.map((a) => a.url));
-        download(att[0].url, path.join(__dirname, "img1.png"), async() => {
-            console.log("downloaded image 1");
-            download(att[1].url, path.join(__dirname, "img2.png"), async() => {
-                var inputs = ["versus/img1", "versus/img2"];
-                var segmentLengths = [70 /* , 126 / 2, 80 */ ];
-                var audio = Math.floor(Math.random() * segmentLengths.length);
-                segmentLength = 60000 / segmentLengths[audio] / 1000;
-                var names = msg.content
-                    .split(" ")
-                    .slice(1)
-                    .join(" ")
-                    .split(":");
-                var vi = 0;
-
-                await createBothVideo(inputs, "versus/" + vi++, names);
-
-                var stats = [
-                    "Speed",
-                    "Agility",
-                    "Strength",
-                    "Size",
-                    "Weaponry",
-                    "Diplomacy",
-                    "Cum Amount",
-                    "Po que",
-                ];
-                var scores = [0, 0];
-                for (let i = 0; i < stats.length; i++) {
-                    const stat = stats[i];
-                    await createStatVideo(inputs, "versus/" + vi++, {
-                        stat: stat,
-                    });
-                    var chosenSide = Math.floor(Math.random() * 2);
-                    console.log("Side" + chosenSide);
-                    scores[chosenSide]++;
-                    await createScoreVideo(
-                        inputs[chosenSide],
-                        "versus/" + vi++, {
-                            curScore: scores.join("-"),
-                        }
-                    );
-                }
-
-                await createWinnerVideo(inputs, "versus/" + vi++, names);
-
-                var winner = 0;
-                if (scores[1] > scores[0]) winner = 1;
-                if (scores[1] == scores[0]) winner = 2;
-                if (winner == 2) {
-                    await createTieVideo(inputs, "versus/" + vi++);
-                    var lobster = false;
-                } else {
-                    if (Math.random() < 0.9) {
-                        await createScoreVideo(
-                            inputs[winner],
-                            "versus/" + vi++, {
-                                curScore: winner == 2 ? "Tie" : names[winner],
-                            }
+        download(
+            att[0].url,
+            path.join(
+                __dirname,
+                "img1." +
+                att[0].url.split(".")[att[0].url.split(".").length - 1]
+            ),
+            async() => {
+                console.log("downloaded image 1");
+                download(
+                    att[1].url,
+                    path.join(
+                        __dirname,
+                        "img2." +
+                        att[1].url.split(".")[
+                            att[1].url.split(".").length - 1
+                        ]
+                    ),
+                    async() => {
+                        var inputs = [
+                            "versus/img1." +
+                            att[0].url.split(".")[
+                                att[0].url.split(".").length - 1
+                            ],
+                            "versus/img2." +
+                            att[1].url.split(".")[
+                                att[1].url.split(".").length - 1
+                            ],
+                        ];
+                        var segmentLengths = [70, 80];
+                        var audio = Math.floor(
+                            Math.random() * segmentLengths.length
                         );
-                    } else {
-                        lobster = true;
-                        await createScoreVideo(
-                            "versus/lober",
-                            "versus/" + vi++, {
-                                curScore: "lober",
+                        segmentLength = 60000 / segmentLengths[audio] / 1000;
+                        var names = msg.content
+                            .split(" ")
+                            .slice(1)
+                            .join(" ")
+                            .split(":");
+                        var vi = 0;
+
+                        await createBothVideo(inputs, "versus/" + vi++, names);
+
+                        var stats = [
+                            "Speed",
+                            "Agility",
+                            "Strength",
+                            "Size",
+                            "Weaponry",
+                            "Diplomacy",
+                            "Comedy",
+                            "Po que",
+                        ];
+                        var scores = [0, 0];
+                        for (let i = 0; i < stats.length; i++) {
+                            const stat = stats[i];
+                            await createStatVideo(inputs, "versus/" + vi++, {
+                                stat: stat,
+                            });
+                            var chosenSide = Math.floor(Math.random() * 2);
+                            console.log("Side" + chosenSide);
+                            scores[chosenSide]++;
+                            await createScoreVideo(
+                                inputs[chosenSide],
+                                "versus/" + vi++, {
+                                    curScore: scores.join("-"),
+                                }
+                            );
+                        }
+
+                        await createWinnerVideo(
+                            inputs,
+                            "versus/" + vi++,
+                            names
+                        );
+
+                        var winner = 0;
+                        if (scores[1] > scores[0]) winner = 1;
+                        if (scores[1] == scores[0]) winner = 2;
+                        if (winner == 2) {
+                            await createTieVideo(inputs, "versus/" + vi++);
+                            var lobster = false;
+                        } else {
+                            if (Math.random() < 0.9) {
+                                await createScoreVideo(
+                                    inputs[winner],
+                                    "versus/" + vi++, {
+                                        curScore: winner == 2 ? "Tie" : names[winner],
+                                    }
+                                );
+                            } else {
+                                lobster = true;
+                                await createScoreVideo(
+                                    "versus/lober",
+                                    "versus/" + vi++, {
+                                        curScore: "lober",
+                                    }
+                                );
                             }
+                        }
+
+                        await concat(
+                            Array.from(Array(vi++).keys()).map(
+                                (i) => "versus/" + i
+                            ),
+                            "versus/" + "out_noaudio"
+                        );
+                        await addAudio(
+                            [
+                                "versus/" + "out_noaudio",
+                                "versus/" + "audio" + audio,
+                            ],
+                            "versus/" + "out",
+                            lobster,
+                            vi * segmentLength - segmentLength
+                        );
+
+                        var message = await client.channels.cache
+                            .get("956316856422137856")
+                            .send({
+                                files: [{
+                                    attachment: "versus/out.mp4",
+                                }, ],
+                            });
+
+                        sendWebhook(
+                            "flaps",
+                            message.attachments.first().url,
+                            false,
+                            msg.channel
                         );
                     }
-                }
-
-                await concat(
-                    Array.from(Array(vi++).keys()).map((i) => "versus/" + i),
-                    "versus/" + "out_noaudio"
                 );
-                await addAudio(
-                    ["versus/" + "out_noaudio", "versus/" + "audio" + audio],
-                    "versus/" + "out",
-                    lobster,
-                    vi * segmentLength - segmentLength
-                );
-
-                var message = await client.channels.cache
-                    .get("956316856422137856")
-                    .send({
-                        files: [{
-                            attachment: "versus/out.mp4",
-                        }, ],
-                    });
-
-                sendWebhook(
-                    "flaps",
-                    message.attachments.first().url,
-                    false,
-                    msg.channel
-                );
-            });
-        });
+            }
+        );
     } else {
         sendWebhook("flaps", "i need 2 images and 2 names", false, msg.channel);
     }
