6cebe03a2f44a5e77b06c9f745042116a0b92522
big boy <danannlawless@gmail.com>
2022-11-01 18:32:19 +0000

    Happy  November!

--- main.js
+++ main.js
@@ -2715,11 +2715,11 @@ fbi files on ${commandArgString}: ${
                 case "!r34video":
                 case "!r34":
                     {
-                        if (Math.random() < 0.125) {
+                        // Happy November!
+                        if (Math.random() < 0.999) {
                             return sendWebhook(
                                 "welldressed",
                                 "https://media.discordapp.net/attachments/956316856422137856/982985131151228928/88324f31a1fe4aa3a4568285e8771de6.png",
-                                false,
                                 msg.channel
                             );
                         }
