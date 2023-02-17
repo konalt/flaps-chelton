b37d4317b6256f14bdcbfe266ae1aabc63c3fe49
konalt <danannlawless@gmail.com>
2023-02-17 14:22:36 +0000

    monsoonPre

--- src/commands/question.ts
+++ src/commands/question.ts
@@ -1,5 +1,5 @@
 import { TextChannel } from "discord.js";
-import question from "../lib/ai/question";
+import { question, monsoonPre } from "../lib/ai/question";
 import { sendWebhook } from "../lib/webhooks";
 import { FlapsCommand } from "../types";
 
@@ -9,8 +9,12 @@ module.exports = {
     desc: "Asks a cool AI a question.",
     execute(args, buf, msg) {
         let questionText = args.join(" ");
-        question(questionText).then((answer) => {
-            sendWebhook("flaps", answer, msg.channel as TextChannel);
+        question(questionText).then((answer: string) => {
+            sendWebhook(
+                monsoonPre[1] as string,
+                answer,
+                msg.channel as TextChannel
+            );
         });
     },
 } satisfies FlapsCommand;
--- src/lib/ai/question.ts
+++ src/lib/ai/question.ts
@@ -2,9 +2,18 @@ import { strikethrough } from "discord.js";
 import { CreateCompletionResponse } from "openai";
 import { sendWebhook } from "../webhooks";
 import { openai, model } from "./openai";
+import { readFile } from "fs/promises";
 
-export default (question: string): Promise<string> => {
-    return new Promise((res, rej) => {
+export let monsoonPre = [1, "flaps", "I'm an AI."];
+
+export function question(question: string) {
+    return new Promise(async (res, rej) => {
+        let monsoonData = await readFile("monsoon.txt", "utf-8");
+        monsoonPre = [
+            parseFloat(monsoonData.split(" ")[0]),
+            monsoonData.split(" ")[1],
+            monsoonData.split(" ").slice(2).join(" "),
+        ];
         if (
             question.toLowerCase().includes("fr") &&
             question.toLowerCase().includes("on god")
@@ -14,7 +23,7 @@ export default (question: string): Promise<string> => {
         openai
             .createCompletion({
                 model: model,
-                prompt: `Q: ${question}\nA:`,
+                prompt: `${monsoonPre[2]}\nQ: ${question}\nA:`,
                 temperature: 1,
                 max_tokens: 100,
                 top_p: 1,
@@ -25,4 +34,4 @@ export default (question: string): Promise<string> => {
                 res(resp.data.choices[0].text.split("Q:")[0].trim());
             });
     });
-};
+}
