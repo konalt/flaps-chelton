13ed55772a6f9b466cba41355f2bdb30266c9001
konalt <danannlawless@gmail.com>
2023-02-18 15:08:07 +0000

    upgraded caption2 to the handleffmpeg func

--- src/commands/caption2.ts
+++ src/commands/caption2.ts
@@ -1,6 +1,7 @@
 import { TextChannel } from "discord.js";
 import { Message } from "discord.js";
 import { ffmpegBuffer } from "../lib/ffmpeg/ffmpeg";
+import handleFFmpeg from "../lib/ffmpeg/handleFFmpeg";
 import { getFileExt, getFileName } from "../lib/utils";
 import { sendWebhook } from "../lib/webhooks";
 import { FlapsCommand } from "../types";
@@ -12,17 +13,13 @@ module.exports = {
     desc: "Adds a Futura caption to a video, image, or gif.",
     needs: ["image/video/gif"],
     async execute(args: string[], buffers: [Buffer, string][], msg: Message) {
-        if (!buffers) return;
         caption2(buffers, {
             text: args.join(" "),
-        }).then((out: Buffer) => {
-            sendWebhook(
-                "ffmpeg",
-                "",
-                msg.channel as TextChannel,
-                out,
-                getFileName("Effect_Caption2", getFileExt(buffers[0][1]))
-            );
-        });
+        }).then(
+            handleFFmpeg(
+                getFileName("Effect_Caption2", getFileExt(buffers[0][1])),
+                msg.channel as TextChannel
+            )
+        );
     },
 } satisfies FlapsCommand;
