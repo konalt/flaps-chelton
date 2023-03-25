710dfe5c92acfddd554a84fe88fb48dd2ae13570
konalt <danannlawless@gmail.com>
2023-03-25 17:31:46 +0000

    !react

---
+++ src/commands/react.ts
@@ -0,0 +1,27 @@
+import { FlapsCommand } from "../types";
+import { makeMessageResp } from "../lib/utils";
+
+module.exports = {
+    id: "react",
+    name: "React",
+    desc: "Reacts to the replied message with a specified emoji.",
+    async execute(args, buf, msg) {
+        return new Promise(async (res) => {
+            var ref = await msg.fetchReference();
+            var emoji = args[0];
+
+            if (!emoji) {
+                return res(makeMessageResp("flaps", "gimme an emoji!"));
+            }
+            if (!ref) {
+                return res(
+                    makeMessageResp("flaps", "reply to smt first silly man!")
+                );
+            }
+
+            ref.react(emoji);
+
+            res(makeMessageResp("", ""));
+        });
+    },
+} satisfies FlapsCommand;
