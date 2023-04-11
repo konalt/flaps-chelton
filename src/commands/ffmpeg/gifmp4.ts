3b230b96510902526dcea4391b6a9d526831ca16
konalt <danannlawless@gmail.com>
2023-04-11 14:54:55 +0100

    !gifmp4

---
+++ src/commands/ffmpeg/gifmp4.ts
@@ -0,0 +1,26 @@
+import { TextChannel } from "discord.js";
+import handleFFmpeg from "../../lib/ffmpeg/handleFFmpeg";
+import reencode from "../../lib/ffmpeg/reencode";
+import { getFileExt, getFileName } from "../../lib/utils";
+import { FlapsCommand } from "../../types";
+import handleFFmpegCatch from "../../lib/ffmpeg/handleFFmpegCatch";
+import gifmp4 from "../../lib/ffmpeg/gifmp4";
+
+module.exports = {
+    id: "gifmp4",
+    name: "GIF -> MP4",
+    desc: "Converts a GIF into an MP4.",
+    needs: ["gif"],
+    execute(args, buf, msg) {
+        return new Promise((res, rej) => {
+            gifmp4(buf).then(
+                handleFFmpeg(
+                    getFileName("Effect_GIFMP4", "mp4"),
+                    msg.channel as TextChannel,
+                    res
+                ),
+                handleFFmpegCatch(msg.channel, res)
+            );
+        });
+    },
+} satisfies FlapsCommand;
