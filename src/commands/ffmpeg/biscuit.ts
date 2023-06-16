a7e1b1ec93ec91127614efc84adefc272eef0520
konalt <danannlawless@gmail.com>
2023-06-16 06:03:18 +0100

    !biscuit

---
+++ src/commands/ffmpeg/biscuit.ts
@@ -0,0 +1,26 @@
+import { TextChannel } from "discord.js";
+import handleFFmpeg from "../../lib/ffmpeg/handleFFmpeg";
+import compress from "../../lib/ffmpeg/compress";
+import { getFileExt, getFileName } from "../../lib/utils";
+import { FlapsCommand } from "../../types";
+import handleFFmpegCatch from "../../lib/ffmpeg/handleFFmpegCatch";
+import { biscuit } from "../../lib/ffmpeg/biscuit";
+
+module.exports = {
+    id: "biscuit",
+    name: "Biscuit",
+    desc: "Literally so insanely biscuit.",
+    needs: ["image"],
+    execute(args, buf, msg) {
+        return new Promise((res, rej) => {
+            biscuit(buf).then(
+                handleFFmpeg(
+                    getFileName("Effect_Biscuit", "mp4"),
+                    msg.channel as TextChannel,
+                    res
+                ),
+                handleFFmpegCatch(msg.channel, res)
+            );
+        });
+    },
+} satisfies FlapsCommand;
---
+++ src/lib/ffmpeg/biscuit.ts
@@ -0,0 +1,20 @@
+import { ffmpegBuffer, file, preset } from "./ffmpeg";
+import { getVideoLength } from "./getVideoDimensions";
+
+export async function biscuit(buffers: [Buffer, string][]) {
+    var len = await getVideoLength(file("biscuit.mp4"));
+    var start_time = 3.67;
+    var length = 0.49;
+
+    console.log(len);
+
+    return ffmpegBuffer(
+        `-loop 1 -t ${len} -i $BUF0 -i ${file(
+            "biscuit.mp4"
+        )} -filter_complex "[0:v]scale=240:240,setsar=1:1,fade=out:st=${start_time}:d=${length}[fader];[1:v][fader]overlay=enable='between(t,${start_time},${
+            start_time + length
+        })'[final]" -shortest -map "[final]" -map "1:a:0" -preset:v ${preset} $OUT`,
+        buffers,
+        "mp4"
+    );
+}
