62858544d95a522b2429b52a45a656d8df9a35e9
konalt <danannlawless@gmail.com>
2023-06-22 19:37:12 +0100

    fix !imgfade duration

--- src/lib/ffmpeg/imgfade.ts
+++ src/lib/ffmpeg/imgfade.ts
@@ -9,9 +9,7 @@ export default async function imgfade(
     return ffmpegBuffer(
         `-i $BUF0 -loop 1 -t ${dur} -i $BUF1 -filter_complex "[1:v]scale=${dims.join(
             ":"
-        )},setsar=1:1,fade=t=in:d=${
-            dur * 2
-        }:alpha=1[s1];[0:v][s1]overlay[out]" -t ${dur} -map "[out]" -map "0:a:0" -preset:v ${preset} $OUT`,
+        )},setsar=1:1,fade=t=in:d=${dur}:alpha=1[s1];[0:v][s1]overlay[out]" -t ${dur} -map "[out]" -map "0:a:0" -preset:v ${preset} $OUT`,
         buffers
     );
 }
