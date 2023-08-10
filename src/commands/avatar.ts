8daaa51ba84609c54cb21c66ccc942e8ac1cc110
konalt <danannlawless@gmail.com>
2023-08-10 23:55:14 +0100

    !avatar

---
+++ src/commands/avatar.ts
@@ -0,0 +1,41 @@
+import { downloadPromise } from "../lib/download";
+import { getFileExt, getFileName, makeMessageResp } from "../lib/utils";
+import { FlapsCommand } from "../types";
+
+module.exports = {
+    id: "avatar",
+    name: "Avatar",
+    desc: "Returns a user's avatar.",
+    async execute(args, buf, msg) {
+        return new Promise(async (res, rej) => {
+            let user = msg.mentions.users.first();
+            if (!user) {
+                if (msg.reference) {
+                    let ref = await msg.fetchReference();
+                    user = ref.author;
+                } else {
+                    user = msg.author;
+                }
+            }
+            let url = user.avatarURL();
+            if (!url) {
+                return res(
+                    makeMessageResp("flaps", "that mf dont have an avatar")
+                );
+            }
+            if (args[0] == "--nogif") {
+                url = url.split(".").slice(0, -1).join(".") + ".png";
+            }
+            const image = await downloadPromise(url);
+            res(
+                makeMessageResp(
+                    "flaps",
+                    "",
+                    null,
+                    image,
+                    getFileName("Avatar", getFileExt(url))
+                )
+            );
+        });
+    },
+} satisfies FlapsCommand;
