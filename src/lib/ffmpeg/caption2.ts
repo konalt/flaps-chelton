cbe0935212b1ee8dcaee99f79fe8378f9d6182bd
konalt <danannlawless@gmail.com>
2023-09-09 17:22:35 +0100

    updated !caption2 to use ffmpegnewbuffer

--- src/lib/ffmpeg/caption2.ts
+++ src/lib/ffmpeg/caption2.ts
@@ -10,7 +10,13 @@ import {
     scheduleDelete,
 } from "../utils";
 import { downloadPromise } from "../download";
-import { ffmpegBuffer, file, preset, usePreset } from "./ffmpeg";
+import {
+    ffmpegBuffer,
+    ffmpegNewBuffer,
+    file,
+    preset,
+    usePreset,
+} from "./ffmpeg";
 import getTextWidth from "../canvas/getTextWidth";
 import { getVideoDimensions } from "./getVideoDimensions";
 
@@ -258,19 +264,24 @@ async function caption2(
         }
     }
     writeFileSync(file(output + ".ffscript"), filter);
-    scheduleDelete(file(output + ".ffscript"), 120);
-    for (const eFile of emojiFileList) {
-        scheduleDelete(eFile, 120);
-    }
-    return ffmpegBuffer(
-        `-y -i $BUF0 ${emojiInputs}-filter_complex_script ${file(
-            output + ".ffscript"
-        )} -map "[out_v]" -map "0:a?" ${usePreset(
-            buffers[0][1]
-        )} -update 1 $OUT`,
-        buffers,
-        getFileExt(buffers[0][1])
-    );
+    return new Promise<Buffer>((resolve, reject) => {
+        ffmpegNewBuffer(
+            `-y -i $BUF0 ${emojiInputs}-filter_complex_script ${file(
+                output + ".ffscript"
+            )} -map "[out_v]" -map "0:a?" $PRESET -update 1 $OUT`,
+            buffers
+        )
+            .then((buffer) => {
+                for (const eFile of emojiFileList) {
+                    scheduleDelete(eFile, 5);
+                }
+                scheduleDelete(file(output + ".ffscript"), 5);
+                resolve(buffer);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 export default caption2;
