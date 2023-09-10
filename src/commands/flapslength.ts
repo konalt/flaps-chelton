d8f64efd10cb781e829af8af9b7b3391e57c6932
konalt <danannlawless@gmail.com>
2023-09-10 14:08:56 +0100

    some changes to !flapslength

--- src/commands/flapslength.ts
+++ src/commands/flapslength.ts
@@ -21,26 +21,16 @@ function globFiles(pattern: string): Promise<string[]> {
 module.exports = {
     id: "flapslength",
     name: "Flaps Length",
-    desc: "Gets the length of all flaps TS files.",
-    execute(args: string[], bufs: [Buffer, string][], msg: Message) {
+    desc: "Gets the length of all flaps source files.",
+    execute(args, bufs, msg) {
         return new Promise((res, rej) => {
-            Promise.all([
-                globFiles("src/**/*.ts"),
-                globFiles("dist/**/*.js"),
-                globFiles("js/**/*.js"),
-            ]).then((files) => {
-                let lineCounts: number[] = new Array(files.length).fill(0);
-                files.forEach((paths, i) => {
-                    paths.forEach((file) => {
-                        lineCounts[i] += getLineCount(file);
-                    });
+            globFiles("src/**/*.ts").then((files) => {
+                let lineCount = 0;
+                files.forEach((file) => {
+                    lineCount += getLineCount(file);
                 });
                 res(
-                    makeMessageResp(
-                        "flaps",
-                        `Lines in TypeScript source: ${lineCounts[0]}\nLines in compiled JS: ${lineCounts[1]}\nLines in old codebase: ${lineCounts[2]}`,
-                        msg.channel as TextChannel
-                    )
+                    makeMessageResp("flaps", `this mf ${lineCount} lines long`)
                 );
             });
         });
