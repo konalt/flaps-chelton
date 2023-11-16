5bc54a3ce6e1932e0eaa73f52ba35523445b83f4
konalt <danannlawless@gmail.com>
2023-11-16 17:51:31 +0000

    commandlist update

--- src/commands/commandlist.ts
+++ src/commands/commandlist.ts
@@ -15,16 +15,16 @@ module.exports = {
             let page = parseInt(args[0]) - 1;
             if (!page) page = 0;
             let pageLength = 8;
-            let pageCount = Math.ceil(commands.size / pageLength);
-            if (page > pageCount - 1)
-                return res(
-                    makeMessageResp(
-                        "flaps",
-                        `theres only ${pageCount} pages silly!`
-                    )
-                );
-            if (page < 0) return res(makeMessageResp("flaps", `what`));
-            let str = Array.from(commands)
+            let commands2 = Array.from(commands)
+                .filter((command) => {
+                    if (
+                        !command[1].showOnCommandSimulator &&
+                        typeof command[1].showOnCommandSimulator == "boolean"
+                    ) {
+                        return false;
+                    }
+                    return true;
+                })
                 .sort((a, b) => {
                     if (a[1].id < b[1].id) {
                         return -1;
@@ -34,7 +34,17 @@ module.exports = {
                     }
                     return 0;
                 })
-                .map((c) => "`!" + c[1].id + "` - " + c[1].desc)
+                .map((c) => "`!" + c[1].id + "` - " + c[1].desc);
+            let pageCount = Math.ceil(commands2.length / pageLength);
+            if (page > pageCount - 1)
+                return res(
+                    makeMessageResp(
+                        "flaps",
+                        `theres only ${pageCount} pages silly!`
+                    )
+                );
+            if (page < 0) return res(makeMessageResp("flaps", `what`));
+            let str = commands2
                 .slice(page * pageLength, (page + 1) * pageLength)
                 .join("\n");
             res(
