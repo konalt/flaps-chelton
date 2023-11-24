1dbc66e67d28e93a851b26022b153baaa9f908ef
konalt <danannlawless@gmail.com>
2023-11-24 17:10:04 +0000

    fix weird ass delay

--- src/lib/ffmpeg/caption2.ts
+++ src/lib/ffmpeg/caption2.ts
@@ -3,6 +3,8 @@ import { getVideoDimensions, getVideoLength } from "./getVideoDimensions";
 import createCaption2 from "../canvas/createCaption2";
 import minimum from "./minimum";
 import { Caption2Options } from "../../types";
+import { getTypeSingular } from "../utils";
+import { lookup } from "mime-types";
 
 export default async function caption2(
     buffers: [Buffer, string][],
@@ -10,9 +12,9 @@ export default async function caption2(
 ): Promise<Buffer> {
     let fixed: [Buffer, string] = [await minimum([buffers[0]]), buffers[0][1]];
     let [width, height] = await getVideoDimensions(fixed);
-    let duration = await getVideoLength(fixed);
-    if (Number.isNaN(duration)) {
-        duration = 0.05; // fucking images
+    let duration = 0;
+    if (getTypeSingular(lookup(fixed[1]) || "text/plain") != "image") {
+        duration = await getVideoLength(fixed);
     }
     let [caption, boxHeight] = await createCaption2(
         width,
@@ -20,11 +22,11 @@ export default async function caption2(
         options.text
     );
     return ffmpegBuffer(
-        `-i $BUF0 -loop 1 -t ${duration} -i $BUF1 -filter_complex "[1:v]pad=ceil(iw/2)*2:ceil(ih/2)*2[even];[even]pad=w=iw:h=ih+${height}[padded];[padded][0:v]overlay=x=0:y=${boxHeight}[overlay];${autoGifPalette(
+        `-i $BUF0 -i $BUF1 -filter_complex "[1:v]pad=ceil(iw/2)*2:ceil(ih/2)*2[even];[even]pad=w=iw:h=ih+${height}[padded];[padded][0:v]overlay=x=0:y=${boxHeight}[overlay];${autoGifPalette(
             "overlay",
             "out",
             fixed[1]
-        )}" -map "0:a?" -map "[out]" -t ${duration} $PRESET $OUT`,
+        )}" -map "0:a?" -map "[out]" -ss 0.05 -to ${duration} $PRESET $OUT`,
         [fixed, [caption, "png"]]
     );
 }
