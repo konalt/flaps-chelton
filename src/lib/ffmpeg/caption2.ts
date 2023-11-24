3caede5b0eb03c6b725fceed60dc91cf3a3faf37
konalt <danannlawless@gmail.com>
2023-11-24 15:29:33 +0000

    new !caption2 minimum size

--- src/lib/ffmpeg/caption2.ts
+++ src/lib/ffmpeg/caption2.ts
@@ -1,14 +1,16 @@
 import { ffmpegBuffer, autoGifPalette } from "./ffmpeg";
 import { getVideoDimensions, getVideoLength } from "./getVideoDimensions";
 import createCaption2 from "../canvas/createCaption2";
+import minimum from "./minimum";
 import { Caption2Options } from "../../types";
 
 export default async function caption2(
     buffers: [Buffer, string][],
     options: Caption2Options
 ): Promise<Buffer> {
-    let [width, height] = await getVideoDimensions(buffers[0]);
-    let duration = await getVideoLength(buffers[0]);
+    let fixed: [Buffer, string] = [await minimum([buffers[0]]), buffers[0][1]];
+    let [width, height] = await getVideoDimensions(fixed);
+    let duration = await getVideoLength(fixed);
     if (Number.isNaN(duration)) {
         duration = 0.05; // fucking images
     }
@@ -21,8 +23,8 @@ export default async function caption2(
         `-i $BUF0 -loop 1 -t ${duration} -i $BUF1 -filter_complex "[1:v]pad=ceil(iw/2)*2:ceil(ih/2)*2[even];[even]pad=w=iw:h=ih+${height}[padded];[padded][0:v]overlay=x=0:y=${boxHeight}[overlay];${autoGifPalette(
             "overlay",
             "out",
-            buffers[0][1]
+            fixed[1]
         )}" -map "0:a?" -map "[out]" -t ${duration} $PRESET $OUT`,
-        [buffers[0], [caption, "png"]]
+        [fixed, [caption, "png"]]
     );
 }
