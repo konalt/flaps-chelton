0f4241115672df88f75ec7139c545e9daa309f57
konalt <danannlawless@gmail.com>
2024-03-13 13:38:06 +0000

    add fps control to videogif function

--- src/lib/ffmpeg/videogif.ts
+++ src/lib/ffmpeg/videogif.ts
@@ -1,8 +1,8 @@
 import { ffmpegBuffer } from "./ffmpeg";
 
-export default function videoGif(buffers: [Buffer, string][]) {
+export default function videoGif(buffers: [Buffer, string][], fps = 16) {
     return ffmpegBuffer(
-        `-i $BUF0 -vf "fps=16,scale=360:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $OUT`,
+        `-i $BUF0 -vf "fps=${fps},scale=360:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $OUT`,
         buffers,
         "gif"
     );
