b8303c80ae91067f314b83756ee3869dde991dd5
konalt <danannlawless@gmail.com>
2024-04-24 17:30:19 +0100

    fixed dimensions for !flag

--- src/lib/ffmpeg/flag.ts
+++ src/lib/ffmpeg/flag.ts
@@ -2,11 +2,14 @@ import { addBufferSequence, removeBuffer } from "../..";
 import { bufferToDataURL } from "../utils";
 import { hookWeb3DAPIAnimation } from "../web3dapi";
 import { ffmpegBuffer } from "./ffmpeg";
+import { getVideoDimensions } from "./getVideoDimensions";
 import videoGif from "./videogif";
 
-async function animate(img: Buffer) {
+async function animate(img: Buffer, dimensions: [number, number]) {
     let animation = await hookWeb3DAPIAnimation("flag", {
         img: bufferToDataURL(img, "image/png"),
+        imgWidth: dimensions[0],
+        imgHeight: dimensions[1],
     });
     let frames: Buffer[] = [];
     for (let i = 1; i < 21; i++) {
@@ -24,7 +27,10 @@ async function animate(img: Buffer) {
 }
 
 export default async function flag(buffers: [Buffer, string][]) {
-    let anim = await animate(buffers[0][0]);
+    let anim = await animate(
+        buffers[0][0],
+        await getVideoDimensions(buffers[0])
+    );
     let gif = await videoGif([[anim, "mp4"]], 24);
     return gif;
 }
