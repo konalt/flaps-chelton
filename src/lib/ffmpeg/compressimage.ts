52f9b44ea27024df5b42e3d4c763a38765b9f43f
konalt <danannlawless@gmail.com>
2024-05-09 15:56:32 +0100

    compress image size fix

--- src/lib/ffmpeg/compressimage.ts
+++ src/lib/ffmpeg/compressimage.ts
@@ -7,7 +7,12 @@ export default async function compressImage(
 ): Promise<Buffer> {
     let dims = await getVideoDimensions(buffer);
     let maxSize = parseInt(process.env.MAX_PERMA_IMAGE_SIZE);
-    let fixedDims = calculateAspectRatioFit(dims[0], dims[1], maxSize, maxSize);
+    let fixedDims = [0, 0];
+    if (Math.max(dims[0], dims[1]) < maxSize) {
+        fixedDims = dims;
+    } else {
+        fixedDims = calculateAspectRatioFit(dims[0], dims[1], maxSize, maxSize);
+    }
     return ffmpegBuffer(
         `-i $BUF0 -vf "scale=${fixedDims[0]}:${fixedDims[1]},setsar=1:1" -q:v 8 $OUT`,
         [buffer],
