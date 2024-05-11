79fbddbb8b43e6634375a9f1ace479e08ebd5926
konalt <danannlawless@gmail.com>
2024-05-11 23:36:48 +0100

    code cleanup

--- src/commands/riggedcoinflip.ts
+++ src/commands/riggedcoinflip.ts
@@ -1,16 +1,11 @@
-import { TextChannel } from "discord.js";
-import { Message } from "discord.js";
 import { makeMessageResp } from "../lib/utils";
-import { sendWebhook } from "../lib/webhooks";
 import { FlapsCommand } from "../types";
 
 module.exports = {
     id: "riggedcoinflip",
     name: "Flip a Coin - Rigged",
     desc: "Simulates a coin flip. Will always be heads.",
-    execute(args: string[], bufs: [Buffer, string][], msg: Message) {
-        return new Promise((res, rej) => {
-            res(makeMessageResp("flaps", "heads", msg.channel as TextChannel));
-        });
+    async execute() {
+        return makeMessageResp("flaps", "heads");
     },
 } satisfies FlapsCommand;
