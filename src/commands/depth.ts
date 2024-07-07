4214de34e0a326d334dcaa951bafdfafb77be2c5
konalt <danannlawless@gmail.com>
2024-07-07 20:28:21 +0100

    !depth

---
+++ src/commands/depth.ts
@@ -0,0 +1,20 @@
+import getDepthMap from "../lib/depth";
+import { getFileName, makeMessageResp } from "../lib/utils";
+import { FlapsCommand } from "../types";
+
+module.exports = {
+    id: "depth",
+    name: "Depth",
+    desc: "Turns an image into a depth map.",
+    needs: ["image"],
+    async execute(_, buffers) {
+        let map = await getDepthMap(buffers[0][0]);
+        return makeMessageResp(
+            "flaps",
+            "",
+            null,
+            map,
+            getFileName("Depth", "png")
+        );
+    },
+} satisfies FlapsCommand;
