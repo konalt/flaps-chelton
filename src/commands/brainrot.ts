4718ab787c7a65898e65bae320e85327b851218a
konalt <danannlawless@gmail.com>
2024-07-16 21:32:59 +0100

    brainrot machine

---
+++ src/commands/brainrot.ts
@@ -0,0 +1,56 @@
+import { makeMessageResp } from "../lib/utils";
+import { FlapsCommand } from "../types";
+import { readFile } from "fs/promises";
+
+function sample(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+module.exports = {
+    id: "brainrot",
+    name: "Brainrot Generator",
+    desc: "brainrot generator",
+    async execute() {
+        let data_raw = (await readFile("./brainrot.txt")).toString();
+        let table = {
+            TEMPLATE: [],
+            NOUN: [],
+            ADJECTIVE: [],
+            LOCATION: [],
+            CHARACTER: [],
+        };
+        let read = "unknown";
+        for (const l of data_raw.split("\n").map((r) => r.trim())) {
+            if (l.length == 0) continue;
+            if (l.startsWith("#")) {
+                read = l.split("#")[1];
+                continue;
+            }
+            if (!table[read]) table[read] = [];
+            table[read].push(l);
+        }
+        let template = sample(table.TEMPLATE);
+        template = template.replace(/%[ncal]+%/g, (s) => {
+            let types = s.split("%")[1].split("");
+            let search = [];
+            for (const t of types) {
+                switch (t) {
+                    case "n":
+                        search.push(...table.NOUN);
+                        break;
+                    case "c":
+                        search.push(...table.CHARACTER);
+                        break;
+                    case "a":
+                        search.push(...table.ADJECTIVE);
+                        break;
+                    case "l":
+                        search.push(...table.LOCATION);
+                        break;
+                }
+            }
+            return sample(search);
+        });
+        return makeMessageResp("flaps", template);
+    },
+} satisfies FlapsCommand;
