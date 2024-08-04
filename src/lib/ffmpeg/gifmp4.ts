c76607d43487d0aeca9589e5c196578193fb884d
konalt <danannlawless@gmail.com>
2024-08-04 20:39:43 +0100

    gifmp4 adds silent audio

--- src/lib/ffmpeg/gifmp4.ts
+++ src/lib/ffmpeg/gifmp4.ts
@@ -2,7 +2,7 @@ import { ffmpegBuffer } from "./ffmpeg";
 
 export default function gifmp4(buffers: [Buffer, string][]) {
     return ffmpegBuffer(
-        `-i $BUF0 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" $PRESET $OUT`,
+        `-i $BUF0 -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -shortest $PRESET $OUT`,
         buffers,
         "mp4"
     );
