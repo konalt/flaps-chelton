7e7136f0217bd6380e6719879c9c355ece8334d7
konalt <danannlawless@gmail.com>
2024-08-19 18:02:30 +0100

    !testtrackreport yesterday

--- src/commands/web3d/testtrackreport.ts
+++ src/commands/web3d/testtrackreport.ts
@@ -9,14 +9,19 @@ module.exports = {
     desc: "Tests the track report feature in the current channel",
     needs: [],
     showOnCommandSimulator: false,
-    execute(_, _1, msg) {
+    execute(args, _1, msg) {
         return new Promise(async (res, rej) => {
             let d = new Date();
+            if (args[0] == "yesterday") {
+                d.setHours(d.getHours() - 24);
+            }
             let dateStr = `${d.getFullYear().toString().padStart(4, "0")}-${(
                 d.getMonth() + 1
             )
                 .toString()
                 .padStart(2, "0")}-${d.getDate().toString().padStart(2, "0")}`;
+            console.log(d);
+
             trackReport(
                 await readFile(`./track/${msg.guildId}/${dateStr}.txt`, "utf-8")
             ).then((img) => {
