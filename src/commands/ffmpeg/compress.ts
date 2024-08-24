d2da0bc9b0291897f539fe2a423384568accd396
konalt <danannlawless@gmail.com>
2024-08-24 20:14:27 +0100

    !compress now redirects

--- src/commands/ffmpeg/compress.ts
+++ src/commands/ffmpeg/compress.ts
@@ -1,19 +1,26 @@
-import { TextChannel } from "discord.js";
 import handleFFmpeg from "../../lib/ffmpeg/handleFFmpeg";
 import compress from "../../lib/ffmpeg/compress";
-import { getFileExt, getFileName } from "../../lib/utils";
+import { getFileExt, getFileName, getTypeSingular } from "../../lib/utils";
 import { FlapsCommand } from "../../types";
 import handleFFmpegCatch from "../../lib/ffmpeg/handleFFmpegCatch";
+import { lookup } from "mime-types";
+import compressJPG from "../../lib/ffmpeg/compressjpg";
 
 module.exports = {
     id: "compress",
     name: "Compress",
-    desc: "Compresses a video a comical amount.",
-    needs: ["video"],
+    desc: "Compresses a video/image a comical amount.",
+    needs: ["video/image"],
     async execute(args, buf, msg) {
         return new Promise((res, rej) => {
-            compress(buf).then(
-                handleFFmpeg(getFileName("Effect_Compress", "mp4"), res),
+            let proc = compress;
+            if (getTypeSingular(lookup(buf[0][1]) || "unknown") == "image")
+                proc = compressJPG;
+            proc(buf).then(
+                handleFFmpeg(
+                    getFileName("Effect_Compress", getFileExt(buf[0][1])),
+                    res
+                ),
                 handleFFmpegCatch(res)
             );
         });
