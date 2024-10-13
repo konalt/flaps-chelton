9a259dc7e277b522bd45805edb66378b47be0937
konalt <danannlawless@gmail.com>
2024-10-13 21:56:07 +0100

    eval updates

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,50 +1,86 @@
-import { makeMessageResp } from "../lib/utils";
-import { sendWebhook } from "../lib/webhooks";
+import { inspect } from "util";
+import { getFileName, makeMessageResp } from "../lib/utils";
 import { FlapsCommand } from "../types";
-import { users } from "../lib/users";
-import { hooks } from "../lib/webhooks";
-import {
-    client,
-    voiceConnections,
-    voicePlayers,
-    addBuffer,
-    addBufferSequence,
-    commands,
-    getAllUserStates,
-} from "../index";
-import fs from "fs/promises";
-import * as utils from "../lib/utils";
+import { readdirSync } from "fs";
 
-const proxy = {
-    users,
-    hooks,
-    fs,
-    sendWebhook,
-    client,
-    voiceConnections,
-    voicePlayers,
-    addBuffer,
-    addBufferSequence,
-    commands,
-    utils,
-    getAllUserStates,
-};
+let lib = { __pop: false };
+async function populate(dir = "") {
+    let o = { __pop: false };
+    for (const file of readdirSync("./dist/lib" + dir, {
+        withFileTypes: true,
+    })) {
+        if (file.isDirectory()) {
+            o[file.name] = await populate(dir + "/" + file.name);
+        } else {
+            let imp = await import("../lib" + dir + "/" + file.name);
+            if (imp.default) {
+                imp = imp.default;
+            }
+            o[file.name.split(".")[0]] = imp;
+        }
+    }
+    o.__pop = true;
+    return o;
+}
 
 module.exports = {
     id: "eval",
     name: "Eval",
     desc: "Evaluates some JavaScript code.",
     showOnCommandSimulator: false,
+    needs: [
+        "image/video/audio/gif?",
+        "image/video/audio/gif?",
+        "image/video/audio/gif?",
+        "image/video/audio/gif?",
+        "image/video/audio/gif?",
+        "image/video/audio/gif?",
+        "image/video/audio/gif?",
+        "image/video/audio/gif?",
+        "image/video/audio/gif?",
+        "image/video/audio/gif?",
+    ],
     async execute(args, bufs, msg) {
+        if (!lib.__pop) {
+            lib = await populate();
+        }
         if (msg.author.id !== process.env.OWNER_TOKEN) {
             return makeMessageResp("flaps", "nuh uh uh!");
         }
+        if (args.join(" ") == "list") {
+            return makeMessageResp("flaps", Object.keys(lib).join(", "));
+        }
+        let outformat = "bin";
+        if (args[0] == "--out") {
+            outformat = args[1];
+            args = args.slice(2);
+        }
         var evaluated = eval(args.join(" "));
         if (evaluated) {
-            return makeMessageResp(
-                "flaps",
-                evaluated.toString ? evaluated.toString() : "No response"
-            );
+            if (evaluated instanceof Promise) {
+                evaluated = await evaluated;
+            }
+            let s = "";
+            if (evaluated instanceof Buffer) {
+                return makeMessageResp(
+                    "flaps",
+                    s,
+                    evaluated,
+                    getFileName("EvalOutput", outformat)
+                );
+            }
+            if (evaluated.toString) {
+                if (evaluated.toString() == "[object Object]") {
+                    s = "``` " + inspect(evaluated) + " ```";
+                } else if (evaluated instanceof Function) {
+                    s = "``` " + evaluated.toString() + " ```";
+                } else {
+                    s = evaluated.toString();
+                }
+            } else {
+                s = "No response";
+            }
+            return makeMessageResp("flaps", s);
         } else {
             return makeMessageResp("flaps", "No response");
         }
