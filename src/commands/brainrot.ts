980942d78fe42b69f3c54dac209a48d923cb6460
konalt <danannlawless@gmail.com>
2025-01-16 10:21:47 +0000

    gender and verbs

--- src/commands/brainrot.ts
+++ src/commands/brainrot.ts
@@ -6,6 +6,12 @@ function sample(arr: any[]) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
+const GENDER = {
+    m: ["he", "him", "his", "himself", "man"],
+    f: ["she", "her", "her", "herself", "woman"],
+    t: ["they", "them", "their", "themselves", "person"],
+};
+
 module.exports = {
     id: "brainrot",
     name: "Brainrot Generator",
@@ -38,49 +44,65 @@ module.exports = {
             template = sample(table.TEMPLATE_SENSUAL);
         }
         let cache: Record<string, string[]> = {};
+        let gendercache: string[] = [];
         template = template.replace(
-            /%[ncalv]+(~\d+)?([\?!])?%/gi,
+            /%[ncalvg]+(~\d+)?([\?!])?(\+\d+)?%/gi,
             (s: string) => {
                 let types = s.split("%")[1].split("~")[0].split("");
-                if (!cache[types.join("")]) cache[types.join("")] = [];
-                let search = [];
-                for (const t of types) {
-                    switch (t.toLowerCase()) {
-                        case "n":
-                            search.push(...table.NOUN);
-                            break;
-                        case "c":
-                            search.push(...table.CHARACTER);
-                            break;
-                        case "a":
-                            search.push(...table.ADJECTIVE);
-                            break;
-                        case "l":
-                            search.push(...table.LOCATION);
-                            break;
-                        case "v":
-                            search.push(...table.VERB);
-                            break;
+                if (types[0] == "g") {
+                    let index = parseInt(
+                        s.split("%")[1].split("~")[1].split("+")[0]
+                    );
+                    let value = "r";
+                    if (gendercache[index]) value = gendercache[index];
+                    if (value == "r") value = sample(Object.keys(GENDER));
+                    let pronounIndex = parseInt(s.split("%")[1].split("+")[1]);
+                    return GENDER[value][pronounIndex];
+                } else {
+                    if (!cache[types.join("")]) cache[types.join("")] = [];
+                    let search = [];
+                    for (const t of types) {
+                        switch (t.toLowerCase()) {
+                            case "n":
+                                search.push(...table.NOUN);
+                                break;
+                            case "c":
+                                search.push(...table.CHARACTER);
+                                break;
+                            case "a":
+                                search.push(...table.ADJECTIVE);
+                                break;
+                            case "l":
+                                search.push(...table.LOCATION);
+                                break;
+                            case "v":
+                                search.push(...table.VERB);
+                                break;
+                        }
                     }
+                    let word = sample(search);
+                    let index = parseInt(s.split("%")[1].split("~")[1]);
+                    if (cache[types.join("")][index]) {
+                        word = cache[types.join("")][index];
+                    } else {
+                        cache[types.join("")].push(word);
+                    }
+                    let isPlural =
+                        s.endsWith("!%") ||
+                        (s.endsWith("?%") && Math.random() > 0.5);
+                    if (isPlural && word.split("/").length > 1) {
+                        word = word.split("/")[1];
+                    } else {
+                        word = word.split("/")[0];
+                    }
+                    if (word.split("@").length > 1) {
+                        gendercache.push(word.split("@")[0]);
+                        word = word.split("@")[1];
+                    }
+                    if (types[0].toUpperCase() == types[0])
+                        word = word.toUpperCase();
+                    return word;
                 }
-                let word = sample(search);
-                let index = parseInt(s.split("%")[1].split("~")[1]);
-                if (cache[types.join("")][index]) {
-                    word = cache[types.join("")][index];
-                } else {
-                    cache[types.join("")].push(word);
-                }
-                let isPlural =
-                    s.endsWith("!%") ||
-                    (s.endsWith("?%") && Math.random() > 0.5);
-                if (isPlural && word.split("/").length > 0) {
-                    word = word.split("/")[1];
-                } else {
-                    word = word.split("/")[0];
-                }
-                if (types[0].toUpperCase() == types[0])
-                    word = word.toUpperCase();
-                return word;
             }
         );
         return makeMessageResp("flaps", template.replace(/\\n/g, "\n"));
