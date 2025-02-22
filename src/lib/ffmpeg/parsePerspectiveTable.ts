99cf1b9baeecdc16920cdbc22d671089e6956a36
konalt <danannlawless@gmail.com>
2025-02-22 00:12:44 +0000

    parsePerspectiveTable now supports lerp

--- src/lib/ffmpeg/parsePerspectiveTable.ts
+++ src/lib/ffmpeg/parsePerspectiveTable.ts
@@ -1,43 +1,98 @@
-import { ffmpegBuffer } from "./ffmpeg";
+import { unlink, writeFile } from "fs/promises";
+import { ffmpegBuffer, file } from "./ffmpeg";
+import { getFileName } from "../utils";
 
 let scalingTableHelpers = ["STR TLX TLY TRX TRY BLX BLY BRX BRY"];
 
-export default function parsePerspectiveTable(
+function lerp(a: number, b: number, v: number) {
+    v = Math.min(1, v);
+    return (1 - v) * a + b * v;
+}
+
+export default async function parsePerspectiveTable(
     txt: string,
     v_width: number,
     v_height: number,
     v_length: number,
     fps: number,
-    in_buf: [Buffer, string]
+    in_buf: [Buffer, string],
+    multiplicative = false,
+    enableLerp = false
 ) {
     let directives = txt
         .split("\n") // split on newlines
         .map((x) => x.trim().replace(/ +/g, " ")) // strip CR if windows, remove duplicate spaces
         .filter((x) => !scalingTableHelpers.includes(x.toUpperCase())) // remove helper lines
-        .map((x) => x.split(" ").map((y) => parseInt(y))); // make array of numbers
+        .map((x) => x.split(" ").map((y) => parseFloat(y))); // make array of numbers
+    if (enableLerp) {
+        let newDirectives = [];
+        let frameCount = directives.at(-1)[0];
+        let lastDirective = directives[0];
+        let nextDirective = directives[1];
+        let nextDirectiveIndex = 1;
+        let directiveDifference = nextDirective[0] - lastDirective[0];
+        let sinceChange = 0;
+        for (let i = 0; i < frameCount; i++) {
+            if (nextDirective[0] == i) {
+                lastDirective = [...nextDirective];
+                nextDirective = directives[++nextDirectiveIndex];
+                if (!nextDirective) nextDirective = directives.at(-1);
+                directiveDifference = nextDirective[0] - lastDirective[0];
+
+                sinceChange = 0;
+            }
+            let newDirective = [i];
+            let j = 1;
+            for (const value of lastDirective.slice(1)) {
+                console.log(value, nextDirective[j]);
+                newDirective.push(
+                    lerp(
+                        value,
+                        nextDirective[j],
+                        sinceChange / directiveDifference
+                    )
+                );
+                j++;
+            }
+            newDirectives.push(newDirective);
+            sinceChange++;
+        }
+        directives = newDirectives;
+    }
     let directiveCount = directives.length;
-    let filter = `color=s=${v_width}x${v_height}:c=black:d=${v_length}:r=${fps},format=rgba[static_bg];[0:v]scale=${v_width}:${v_height},setsar=1:1,split=${directiveCount}`;
+    let filter = `color=s=${v_width}x${v_height}:c=black:d=${v_length}:r=${fps},format=rgba[b];[0:v]scale=${v_width}:${v_height},setsar=1:1,split=${directiveCount}`;
     for (let i = 0; i < directiveCount; i++) {
-        filter += `[i_${i}]`;
+        filter += `[${i}]`;
     }
     filter += ";";
     for (let i = 0; i < directiveCount; i++) {
-        filter += `[i_${i}]perspective=${directives[i]
+        let isY = false;
+        filter += `[${i}]perspective=${directives[i]
             .slice(1)
-            .join(":")}:sense=1[p_${i}];`;
+            .map((a) => {
+                isY = !isY;
+                return multiplicative
+                    ? Math.round(a * (isY ? v_height : v_width))
+                    : a;
+            })
+            .join(":")}:sense=1[_${i}];`;
     }
-    filter += "[static_bg]";
+    filter += "[b]";
     for (let i = 0; i < directiveCount; i++) {
         let end = v_length * fps;
         if (i < directiveCount - 1) {
             end = directives[i + 1][0];
         }
-        filter += `[p_${i}]overlay=0:0:enable='between(n\\,${directives[i][0]}\\,${end})'[static_bg_a${i}];[static_bg_a${i}]`;
+        filter += `[_${i}]overlay=0:0:enable='between(n\\,${directives[i][0]}\\,${end})'[a${i}];[a${i}]`;
     }
     filter += "null[scaling_result]";
-    return ffmpegBuffer(
-        `-r ${fps} -t ${v_length} -i $BUF0 -filter_complex "${filter}" -map "[scaling_result]" -t ${v_length} -r ${fps} $PRESET $OUT`,
+    let scriptname = file("cache/" + getFileName("Perspective", "ffscript"));
+    await writeFile(scriptname, filter);
+    let out = await ffmpegBuffer(
+        `-r ${fps} -t ${v_length} -i $BUF0 -filter_complex_script ${scriptname} -map "[scaling_result]" -t ${v_length} -r ${fps} $PRESET $OUT`,
         [in_buf],
         "mp4"
     );
+    await unlink(scriptname);
+    return out;
 }
