5fcbab2f8ddf7a96ca2e6265b1573801d195c1c0
konalt <danannlawless@gmail.com>
2025-03-05 13:39:32 +0000

    define cleanup

--- src/lib/define.ts
+++ src/lib/define.ts
@@ -1,23 +1,15 @@
 import fetch from "node-fetch";
 
-export default function define(word: string): Promise<string> {
-    return new Promise((res, rej) => {
-        fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + word)
-            .then((r) => r.json())
-            .then((r) => {
-                let definition = r[0];
-                if (!definition) {
-                    return res('Unknown Word "' + word + '"');
-                }
-                res(
-                    definition.word +
-                        "\n**" +
-                        definition.phonetic +
-                        "**\n" +
-                        definition.meanings
-                            .map((m: any) => m.definitions[0].definition)
-                            .join("\n")
-                );
-            });
-    });
+export default async function define(word: string): Promise<string> {
+    let response = await fetch(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+    ).then((r) => r.json());
+    let definition = response[0];
+    if (!definition) return `Unknown word ${word}`;
+    let meanings = definition.meanings.map(
+        (m: any) => m.definitions[0].definition
+    );
+    return `${definition.word}\n**${definition.phonetic}**\n${meanings.join(
+        "\n"
+    )}`;
 }
