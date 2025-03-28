6f019b0c43baae09303cc80eddedf2b1e7d295cd
konalt <danannlawless@gmail.com>
2025-03-28 13:17:37 +0000

    new minimum function

--- src/lib/ffmpeg/minimum.ts
+++ src/lib/ffmpeg/minimum.ts
@@ -1,8 +1,8 @@
 import { autoGifPalette, ffmpegBuffer } from "./ffmpeg";
 
-export default function minimum(buffers: [Buffer, string][]) {
+export default function minimum(buffers: [Buffer, string][], size = 368) {
     return ffmpegBuffer(
-        `-i $BUF0 -filter_complex "[0:v]scale=-2:h=max(ih\\,368)[sc];${autoGifPalette(
+        `-i $BUF0 -filter_complex "[0:v]scale=-2:h=max(ih\\,${size})[sc];${autoGifPalette(
             "sc",
             "out",
             buffers[0][1]
