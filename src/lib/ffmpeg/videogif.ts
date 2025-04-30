67f2d4a648c8af8d121a4543c9e156786eed0259
konalt <danannlawless@gmail.com>
2025-04-30 20:40:46 +0100

    videogif change

--- src/lib/ffmpeg/videogif.ts
+++ src/lib/ffmpeg/videogif.ts
@@ -1,8 +1,14 @@
 import { ffmpegBuffer } from "./ffmpeg";
 
-export default function videoGif(buffers: [Buffer, string][], fps = 16) {
+export default function videoGif(
+    buffers: [Buffer, string][],
+    fps = 16,
+    scale = true
+) {
     return ffmpegBuffer(
-        `-i $BUF0 -vf "fps=${fps},scale=360:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -r ${fps} -loop 0 $OUT`,
+        `-i $BUF0 -vf "fps=${fps}${
+            scale ? ",scale=360:-1:flags=lanczos" : ""
+        },split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -r ${fps} -loop 0 $OUT`,
         buffers,
         "gif"
     );
