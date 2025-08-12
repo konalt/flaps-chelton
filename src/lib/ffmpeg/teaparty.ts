2f7da71950f40c1acfae4e26e1af1856692d13f2
konalt <danannlawless@gmail.com>
2025-08-12 17:30:35 +0100

    at least it works

--- src/lib/ffmpeg/teaparty.ts
+++ src/lib/ffmpeg/teaparty.ts
@@ -14,7 +14,7 @@ export default async function yababaina(image: Buffer) {
             `-loop 1 -r ${fps} -i $BUF0`,
             `-i ${file("teaparty.mp3")}`,
             `-filter_complex "[0:v]split=2[bg_base][bg_flipped];[bg_flipped]hflip[bg_flipped];[bg_base][bg_flipped]overlay=0:0:enable='gt(mod(n\\,45)\\,22)'[bg];`,
-            `color=c=0xffffffff:s=${w}x${h}:r=${fps}:d=${duration}[white];color=c=0xffffff00:s=${w}x${h}:r=${fps}:d=${duration}[white_transparent];`,
+            //`color=c=0xffffffff:s=${w}x${h}:r=${fps}:d=${duration}[white];color=c=0xffffff00:s=${w}x${h}:r=${fps}:d=${duration}[white_transparent];`,
             `[1:a]anull[out_audio];[bg]null[out_final]"`,
             `-map "[out_audio]" -map "[out_final]" -r ${fps} -t ${duration} $PRESET $OUT`,
         ].join(" "),
