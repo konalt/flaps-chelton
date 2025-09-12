92e745a1afd6db80307de38a120b628ad79327e0
konalt <danannlawless@gmail.com>
2025-09-13 00:47:07 +0100

    hueshift works on pngs

--- src/lib/ffmpeg/hueshift.ts
+++ src/lib/ffmpeg/hueshift.ts
@@ -1,5 +1,7 @@
 import { ffmpegBuffer } from "./ffmpeg";
 
 export default function invert(buffer: [Buffer, string], amount = 180) {
-    return ffmpegBuffer(`-i $BUF0 -vf hue=${amount} $PRESET $OUT`, [buffer]);
+    return ffmpegBuffer(`-i $BUF0 -vf format=rgba,hue=${amount} $OUT`, [
+        buffer,
+    ]);
 }
